22b33b492e5dd64a9f5423dd699d17e9dd6e2aec
Clara <clara.carbonetti@gmail.com>
2024-10-03 20:28:39 -0300

    Add files via upload

---
+++ Ingresos_1er_Semestre.py
@@ -0,0 +1,9 @@
+ingreso_Enero = 5520.3
+ingreso_Febrero = 3526.5
+ingreso_Marzo = 8540
+ingreso_Abril = 10000
+ingreso_Mayo = 15000
+ingreso_Junio = 7890
+Ingreso_Promedio = (ingreso_Enero+ingreso_Febrero+ingreso_Marzo+ingreso_Abril+ingreso_Mayo+ingreso_Junio) /6
+print(("El ingreso promedio de los primeros 6 meses del año es:"), str (Ingreso_Promedio))
+
